b62db93cbb0ba637b58181c43f44bf681738f16c
Renam Gustavo Bulhoes <renamgustavo@live.com>
2023-03-12 13:38:05 -0300

    Initial

--- creational-patterns-ref/src/main/java/br/com/cod3r/builder/implementationpessoal/Main.java
+++ creational-patterns-ref/src/main/java/br/com/cod3r/builder/implementationpessoal/Main.java
@@ -1,2 +1,22 @@
-package br.com.cod3r.builder.implementationpessoal;public class Main {
+package br.com.cod3r.builder.implementationpessoal;
+
+import br.com.cod3r.builder.implementationpessoal.model.Product;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        Product p1 = new Product.Builder("Caderno")
+                .withBrand("Panine")
+                .build();
+
+        Product p2 = new Product.Builder("Carrinho de controle remoto")
+                .withCategory("Brinquedo")
+                .withAvaliableAmount(2)
+                .withPrice(BigDecimal.valueOf(100))
+                .build();
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
 }
--- creational-patterns-ref/src/main/java/br/com/cod3r/builder/implementationpessoal/model/Product.java
+++ creational-patterns-ref/src/main/java/br/com/cod3r/builder/implementationpessoal/model/Product.java
@@ -1,2 +1,100 @@
-package br.com.cod3r.builder.implementationpessoal.model;public class Product {
+package br.com.cod3r.builder.implementationpessoal.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private final String name;
+    private final String description;
+    private final String brand;
+    private final String category;
+    private final int avaliableAmount;
+    private final BigDecimal price;
+
+    private Product(String name, String description, String brand, String category, int avaliableAmount, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.category = category;
+        this.avaliableAmount = avaliableAmount;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getAvaliableAmount() {
+        return avaliableAmount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public static class Builder {
+        private final String name;
+        private String description;
+        private String brand;
+        private String category;
+        private int avaliableAmount;
+        private BigDecimal price;
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder withBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Builder withCategory(String category) {
+            this.category = category;
+            return this;
+        }
+
+        public Builder withAvaliableAmount(int avaliableAmount) {
+            this.avaliableAmount = avaliableAmount;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(name, description, brand, category, avaliableAmount, price);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", brand='" + brand + '\'' +
+                ", category='" + category + '\'' +
+                ", avaliableAmount=" + avaliableAmount +
+                ", price=" + price +
+                '}';
+    }
 }
